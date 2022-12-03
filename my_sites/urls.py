10a5b43fe6bdf6999118007fb02eed3da904b80b
unknown <baigulov_a@auca.kg>
2022-12-04 00:42:10 +0600

    little changes

--- my_sites/urls.py
+++ my_sites/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='templates/signin.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 if settings.DEBUG:
